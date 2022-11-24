53567e18a1be19ff16b0809fa7c2aab56def56c5
Aidan Traboulay <aidantraboulay13@gmail.com>
2022-11-24 07:01:30 -0500

    Add solution to Two Sum (py)

---
+++ src/1 Two Sum/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums, target):
+        h = {}
+        
+        for i, num in enumerate(nums):
+            n  = target -num
+            
+            if n not in h:
+               h[num] = i
+            
+            else:
+               return[h[n], i]
+
+if __name__ == "__main__":
+     s = Solution()
+     print(s.twoSum([2,7,11,15], 9))
